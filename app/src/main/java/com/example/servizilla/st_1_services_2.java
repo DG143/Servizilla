cf7e66432806e36230631a78d42711bd42c0e89e
DG143 <goeldeep11@gmail.com>
2022-06-14 21:57:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/servizilla/st_1_services_2.java
@@ -0,0 +1,14 @@
+package com.example.servizilla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class st_1_services_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_st1_services2);
+    }
+}
